85547aab4c8b23ff775527185f9967544a69eeb6
Timothy O <timoliver200@gmail.com>
2024-05-04 20:41:42 -0400

    incorporated split pipelines (recommend we get original pipeline id too

--- gcloud_functions/main.py
+++ gcloud_functions/main.py
@@ -10,6 +10,18 @@ from zipfile import ZipFile
 import functions_framework as ff
 
 DEBUG = True
+# get original zip folder over enhanced geojson
+ZIP = False
+
+### other variables that might change based on environment
+zipPath = "./data/"
+geojsonPath = zipPath+"TX_NGPipe.geojson"
+ngPipePath = zipPath+"TX_NGPipe/NaturalGas_Pipelines_TX.shp"
+
+metersPerUnit = (1.11/0.00001)
+FHmeters = 500/metersPerUnit
+i_date = '2022-05-01'
+f_date = '2022-06-30'
 
 
 def mapImg(img, lon, lat):
@@ -22,6 +34,7 @@ def mapImg(img, lon, lat):
 
     Map.setControlVisibility()
     return Map
+
 
 def getSHP():
     
@@ -36,6 +49,22 @@ def getSHP():
 
     with ZipFile(destFile, 'r') as zipped:
         zipped.extractall("./data")
+
+
+def getGJN():
+    
+    destFile = "./data/TX_NGPipe.geojson"
+    if not(os.path.exists(destFile)):
+        os.makedirs("./data/",exist_ok=True)
+
+    blob = storage.Client(
+        project="pipeline-elevation-project").bucket(
+        "pipeline_data_bucket").blob("raw/pl_segments.geojson")
+    blob.download_to_filename(destFile)
+    
+
+def findPiece(id, subset):
+    return list(subset[subset.unique_id==id].index)[0]
     
 
 @ff.http
@@ -47,21 +76,20 @@ def main(request):
     except:
         ee.Initialize(project="pipeline-elevation-project")
 
-    getSHP()
+    aerColl = ee.ImageCollection("USDA/NAIP/DOQQ")
+    orthColl = ee.ImageCollection("SKYSAT/GEN-A/PUBLIC/ORTHO/RGB")    
 
-    zipPath = "./data/"
-    ngPipePath = zipPath+"TX_NGPipe/NaturalGas_Pipelines_TX.shp"
-    ngPipes = gpd.read_file(ngPipePath)
+    if ZIP:
+        getSHP()
+        ngPipes = gpd.read_file(ngPipePath)
+        desc = "pipeline"
+    else:
+        getGJN()
+        ngPipes = gpd.read_file(geojsonPath)
+        desc = "enhanced_pipeline"
     MercNGPipes = ngPipes.to_crs(epsg=4326)
     MercNGPipes["centroids"] = MercNGPipes.geometry.centroid.to_crs(epsg=4326)
     MercNGPipes["beg"] = MercNGPipes.geometry.boundary.centroid.to_crs(epsg=4326)
-
-    metersPerUnit = (1.11/0.00001)
-    FHmeters = 500/metersPerUnit
-    aerColl = ee.ImageCollection("USDA/NAIP/DOQQ")
-    orthColl = ee.ImageCollection("SKYSAT/GEN-A/PUBLIC/ORTHO/RGB")
-    i_date = '2022-05-01'
-    f_date = '2022-06-30'
     retrievedPics = 0
     # TX rectangle bounds
     # tx = ee.Geometry.Rectangle(-106.64719063660635,25.840437651866516,-93.5175532104321,36.50050935248352)
@@ -75,6 +103,15 @@ def main(request):
         # bbox args: west, south, east, north
         bbox = ee.Geometry.BBox(lon-FHmeters, lat-FHmeters, lon+FHmeters, 
                                 lat+FHmeters)
+        # get pipeline_id if from geojson
+        descAdd = ""
+        '''UNCOMMENT ONCE PREPPED'''
+        # if not(ZIP):
+        #     descAdd = str(MercNGPipes.pipeline_id[i:i+1].squeeze())+"-"+str(findPiece(
+        #         id = MercNGPipes.unique_id[i:i+1].squeeze(),
+        #         subset = MercNGPipes.loc[MercNGPipes.pipeline_id==MercNGPipes.pipeline_id[i:i+1].squeeze()]
+        #     ))
+
 
         # get image(s) within box
         aList = aerColl.select('R', 'G', 'B', 'N').filterDate(i_date, f_date).filterBounds(bbox)
@@ -85,16 +122,16 @@ def main(request):
         aImg = ee.Image(aList.toList(100).get(1))
         try:
             mapImg(aImg, lon, lat)
-            ## Export section
+            # Export section
             task = ee.batch.Export.image.toCloudStorage(aImg,
-                                    description=f"USDA_aerials/pipeline{i}",  # put part of pipeline here too
+                                    description=f"USDA_aerials/{desc}_{descAdd}",  # put part of pipeline here too
                                     # ^ put desired name for the task & file in cloud storage
                                     bucket = "gee_image_exports",  # should be gee_image_exports
                                     region=bbox,  # I wonder if filtering by box first works too
                                     #maxPixels=1500000000  
                                     )
             task.start()
-            ##### to view proper map
+            # to view proper map
             print(f"Aerial Ag {i}")
             retrievedPics += 1
         except:
@@ -105,16 +142,16 @@ def main(request):
         oImg = ee.Image(oList.toList(100).get(1))
         try:
             mapImg(oImg, lon, lat)
-            ## Export section
+            # Export section
             task = ee.batch.Export.image.toCloudStorage(oImg,
-                                    description=f"SKYSAT_ortho/pipeline{i}",  # put part of pipeline here too
+                                    description=f"SKYSAT_ortho/{desc}_{descAdd}",  # put part of pipeline here too
                                     # ^ put desired name for the task & file in cloud storage
                                     bucket = "gee_image_exports",  # should be gee_image_exports
                                     region=bbox,  # I wonder if filtering by box first works too
                                     #maxPixels=1500000000  
                                     )
             task.start()
-            ##### to view proper map
+            # to view proper map
             print(f"Aerial Orthro {i}")
             retrievedPics += 1
             continue
@@ -123,7 +160,9 @@ def main(request):
                 print(f"skipped ortho pipe {i}")
             pass
 
-    return f"Successfully retrieved {retrievedPics} images out of {len(MercNGPipes)} total records."
+    return f"Successfully retrieved {retrievedPics} images out of " + \
+        f"{len(MercNGPipes)} total records."
 
-    if __name__ == "__main__":
-        main("sample")
+
+if __name__ == "__main__":
+    main("sample")
